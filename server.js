cf1ffe2f710ec4447ba5522ea8a827eefa629765
Alan Duarte <133732698+dev-alanduarte@users.noreply.github.com>
2023-12-29 16:55:32 -0300

    aaaaaa

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require("fs/promises");
 
 const app = express();
 const apiUrl = "https://ws.hubdodesenvolvedor.com.br/v2/cpf/";
-
+const situationalApiUrl = "http://34.171.167.8/api/tools/search-cpf/";
 const fixedToken = "132747645BVjxnELSKr239671848";
 
 app.use(express.json());
@@ -22,7 +22,7 @@ function getCurrentTimestamp() {
 async function logSuccessfulRequest(cpf) {
   const timestamp = getCurrentTimestamp();
   const logMessage = `Requisição bem-sucedida para CPF ${cpf} em ${timestamp}\n`;
-  
+
   try {
     await fs.appendFile("successful_requests.log", logMessage);
   } catch (err) {
@@ -35,52 +35,60 @@ app.post("/verificar-cpf", async (req, res) => {
     const { cpf, dataNascimento } = req.body;
     const apiRequestUrl = `${apiUrl}?cpf=${cpf}&data=${dataNascimento}&token=${fixedToken}`;
 
-    const response = await axios.get(apiRequestUrl);
+    let response;
+    try {
+      response = await axios.get(apiRequestUrl);
+    } catch (error) {
+      console.error("Erro na primeira API:", error);
+      throw error;
+    }
 
-    if (response.status === 200) {
-      if (response.data.status) {
-        successfulRequests++;
-        console.log(`Requisições bem-sucedidas: ${successfulRequests}`);
-        processedData.add(cpf);
+    if (response.status === 200 && response.data.status) {
+      successfulRequests++;
+      console.log(`Requisições bem-sucedidas: ${successfulRequests}`);
+      processedData.add(cpf);
 
-        const apiUrl2 = "http://34.171.167.8/api/tools/search-cpf/";
+      // Formate a data para dd/mm/yy
+      const formattedDateNascimento = new Date(response.data.dataNascimento).toLocaleDateString("pt-BR");
 
+      const situationalStatusText = response.data.result.situacao_cadastral;
+
+      if (situationalStatusText && situationalStatusText.toUpperCase().includes("REGULAR")) {
+        await logSuccessfulRequest(cpf);
+        
+        // Chame a segunda API
+        const situationalApiRequestUrl = `${situationalApiUrl}${cpf}`;
+        let situationalApiResponse;
         try {
-          const response2 = await axios.get(apiUrl2 + cpf);
-
-          if (response2.status === 200) {
-            // Verifica se a situacao_cadastral é "REGULAR"
-            if (response.data.result.situacao_cadastral === "REGULAR") {
-              await logSuccessfulRequest(cpf, response.data, response2.data);
-              res.json({ data: response.data, dataFromApi2: response2.data });
-            } else {
-              res.status(402).json({ error: "CPF IRREGULAR" });
-            }
-          } else {
-            throw new Error(`Erro HTTP! status: ${response2.status}`);
-          }
+          situationalApiResponse = await axios.get(situationalApiRequestUrl);
         } catch (error) {
-          handleApiError(error, res);
+          console.error("Erro na segunda API:", error);
+          throw error;
         }
+        // Adicione a resposta da segunda API ao objeto retornado
+        res.header("Content-Type", "application/json").json({ data: response.data, dataFromApi2: situationalApiResponse.data });
       } else {
-        // Situation is irregular, send a specific message
-        res.status(400).json({ error: "CPF Inválido ou Data de Nascimento " });
+        console.log("Situação cadastral da primeira API (erro):", situationalStatusText);
+        res.status(402).header("Content-Type", "application/json").json({
+          error: `CPF IRREGULAR - Situação cadastral: ${situationalStatusText || "Não disponível"}`,
+          dataFromApi2: {},
+        });
       }
     } else {
-      throw new Error(`Erro HTTP! status: ${response.status}`);
+      console.log("Resposta da primeira API (erro):", response.data);
+      res.status(400).header("Content-Type", "application/json").json({ error: "CPF Inválido ou Data de Nascimento inválida" });
     }
   } catch (error) {
+    console.error("Erro geral:", error);
     handleApiError(error, res);
   }
 });
 
 function handleApiError(error, res) {
   if (error.code === "ECONNRESET") {
-    res.status(502).json({ error: "502 Bad Gateway - Conexão Resetada" });
+    res.status(502).header("Content-Type", "application/json").json({ error: "502 Bad Gateway - Conexão Resetada" });
   } else {
-    res
-      .status(error.response ? error.response.status : 500)
-      .json({ error: "Erro ao verificar dados." });
+    res.status(500).header("Content-Type", "application/json").json({ error: "Erro interno no servidor" });
   }
 }
 
